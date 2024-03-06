3538aa56ed5ee0b2f4d0bfeee7dbee6f3c576129
Ivan Leonardi <ivanthaisjesus@gmail.com>
2024-03-06 00:42:55 -0300

    alter port

--- src/server.js
+++ src/server.js
@@ -39,7 +39,7 @@ app.use((error, req, res, next) => {
     });
 });
 
-const PORT = process.env.PORT || 3333;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
     console.log(`Server is running on Port ${PORT} 🚀`);
