204b17c03ea4a3e1c9833796c13678cef14f7260
Ivan Leonardi <ivanthaisjesus@gmail.com>
2024-03-17 23:11:14 -0300

    updated cors

--- src/server.js
+++ src/server.js
@@ -11,16 +11,33 @@ import connection from "./database/knex/index.js";
 
 const app = express();
 
-app.use(cors({
-  origin: [
-    "https://sgpdev.vercel.app/",
-    "https://sgpdev.vercel.app/register",
-    "https://sgpdev.vercel.app/users",
-    "https://sgpdev.vercel.app/projects",
-    "https://sgpdev.vercel.app/sessions",
-    "https://sgpdev.vercel.app/developers"
-  ] 
-}));
+app.use((req, res, next) => {
+  // Qualquer endereço pode fazer requisição
+  res.header("Access-Control-Allow-Origin", "*");
+
+  // Tipo de métodos que a API aceita
+  res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+
+  // Permitir o envio de dados para a API
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+
+  //Executar o cors
+  app.use(cors());
+
+  // Quando não houver erro deve continuar o processamento
+  next();
+});
+
+// app.use(cors({
+//   origin: [
+//     "https://sgpdev.vercel.app/",
+//     "https://sgpdev.vercel.app/register",
+//     "https://sgpdev.vercel.app/users",
+//     "https://sgpdev.vercel.app/projects",
+//     "https://sgpdev.vercel.app/sessions",
+//     "https://sgpdev.vercel.app/developers"
+//   ]
+// }));
 
 app.use(express.json());
 
